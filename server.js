1789249cde50936d9c3ca88c1145d5e8c7e49d0a
Suthayachandran <suthayachandran@myseneca.ca>
2026-04-07 10:16:27 -0400

    final mongo fix

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const exphbs = require("express-handlebars");
 const sessions = require("client-sessions");
-const LineByLineReader = require("line-by-line");
-
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+mongoose.connect("mongodb+srv://suthayachandran:sush123@cluster0.06cl7ud.mongodb.net/?appName=Cluster0")
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+const imageSchema = new mongoose.Schema({
+  filename: String
+});
+
+const Image = mongoose.model("Image", imageSchema);
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
@@ -29,25 +38,15 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 // read users
-const users = JSON.parse(fs.readFileSync(path.join(__dirname, "user.json")));
+const users = JSON.parse(
+  fs.readFileSync(path.join(__dirname, "user.json"))
+);
 
-// function to read images from imageList.txt
-function getImages(callback) {
-
-  const filePath = path.join(__dirname, "imageList.txt");
-
-  fs.readFile(filePath, "utf8", (err, data) => {
-    if (err) {
-      console.log("Error reading file:", err);
-      return callback([]);
-    }
-
-    const images = data.split("\n").map(line => line.trim()).filter(line => line !== "");
-
-    callback(images);
-  });
-
+// function to get images from MongoDB
+async function getImages() {
+  return await Image.find();
 }
+
 
 // login page
 app.get("/", (req, res) => {
@@ -68,45 +67,42 @@ app.post("/login", (req, res) => {
   }
 
   req.session.user = username;
-
   res.redirect("/gallery");
 });
 
 // gallery page
-app.get("/gallery", (req, res) => {
+app.get("/gallery", async (req, res) => {
   if (!req.session.user) {
     return res.redirect("/");
   }
 
-  getImages((images) => {
-    res.render("gallery", {
-      username: req.session.user,
-      images: images,
-      selectedImage: images[0]
-    });
+  const images = await getImages();
+
+  res.render("gallery", {
+    username: req.session.user,
+    images: images.map(img => img.filename),
+    selectedImage: images[0]?.filename
   });
 });
 
 // handle image selection
-app.post("/gallery", (req, res) => {
+app.post("/gallery", async (req, res) => {
   if (!req.session.user) {
     return res.redirect("/");
   }
 
-  const chosen = req.body.image;
+  const images = await getImages();
 
-  getImages((images) => {
-    let imageToShow = chosen;
+  let imageToShow = req.body.image;
 
-    if (!imageToShow) {
-      imageToShow = images[0];
-    }
+  if (!imageToShow) {
+    imageToShow = images[0]?.filename;
+  }
 
-    res.render("gallery", {
-      username: req.session.user,
-      images: images,
-      selectedImage: imageToShow
-    });
+  res.render("gallery", {
+    username: req.session.user,
+    images: images.map(img => img.filename),
+    selectedImage: imageToShow
   });
 });
 
@@ -117,6 +113,7 @@ app.get("/logout", (req, res) => {
 });
 
 // start server
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.get("/logout", (req, res) => {
+  req.session.reset();
+  res.redirect("/");
 });
